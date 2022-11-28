38f253b707c58ba3d30f17400f2319bc765693ef
IlesanmiEster <hadassah4u2nv@gmail.com>
2022-11-28 09:10:43 +0100

    Java Task14

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is my first Java project");
+        System.out.println("I have learnt so much on this course");
+        System.out.println("At first I thought Software Testing will be so difficult but Testify did the magic");
+        System.out.println("Testify has made Software Testing so easy to learn for me");
+        System.out.println("I can now boast as a Tester");
+        System.out.println("Enjoying every lesson so far");
+        System.out.println("I can now boast as a tester");
+        System.out.println("Thanks to team Postman, Thanks to Testify");
+    }
+
+}
